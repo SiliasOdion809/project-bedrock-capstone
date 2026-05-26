882b55ecd917f0bd22ff00f054b2595639848940
SiliasOdion809 <siliasodion809@gmail.com>
2026-05-26 21:49:57 +0100

    Updated output.tf

--- terraform/outputs.tf
+++ terraform/outputs.tf
@@ -13,3 +13,23 @@ output "vpc_id" {
 output "region" {
   value = "us-east-1"
 }
+
+output "mysql_endpoint" {
+  value = module.rds.mysql_endpoint
+}
+
+output "postgres_endpoint" {
+  value = module.rds.postgres_endpoint
+}
+
+output "mysql_db_instance_id" {
+  value = module.rds.mysql_db_instance_id
+}
+
+output "postgres_db_instance_id" {
+  value = module.rds.postgres_db_instance_id
+}
+
+output "rds_security_group_id" {
+  value = module.rds.rds_security_group_id
+}
