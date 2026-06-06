2d249947911895cadfa2d4ece0c9539484496257
SiliasOdion809 <siliasodion809@gmail.com>
2026-06-06 18:34:25 +0100

    Testing automated deployment

--- terraform/dynamodb/main.tf
+++ terraform/dynamodb/main.tf
@@ -1,10 +1,26 @@
 resource "aws_dynamodb_table" "orders" {
   name         = var.table_name
   billing_mode = "PAY_PER_REQUEST"
-  hash_key     = "id"
+
+  hash_key = "id"
 
   attribute {
     name = "id"
     type = "S"
+  }
+
+  attribute {
+    name = "customerId"
+    type = "S"
+  }
+
+  global_secondary_index {
+    name            = "idx_global_customerId"
+    hash_key        = "customerId"
+    projection_type = "ALL"
+  }
+
+  tags = {
+    Name = var.table_name
   }
 }
